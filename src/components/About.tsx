bf966166119cce6f5357772a857c7f719d4ffaa0
absolute <boleii466@gmail.com>
2025-06-21 12:25:52 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -38,12 +38,12 @@ export const About = () => (
             </p>
             <ul>
                 <li>Independent Malware Researcher and Analyst, capturing malware strains on a <a href="https://github.com/telekom-security/tpotce" target="_blank">T-POT</a>, with pre-configured Kippo, Dionaea, Cowrie.</li>
-                <li>Elected as a member of Team Slovakia 🇸🇰 , primarily solving reverse engineering tasks with a secondary focus on crypto/forensics.</li>
-                <li>Founded and captained rakuz4n, a CTF team consisting of players from teams from all across the world, with the intent on creating a friendly enviroment for collective learning and individual growth.</li>
-                <li>Closely worked with private security researchers and veteran red teamers on the topics of penetration testing and malware development, where we specifically worked on innovative methods to extract SSNs without relying on incrementation and parsing the NTDLL.dll along with anti-sandbox techniques. One of our biggest researches was the attempted identification and termination of all PatchGuard threads, in order to allow tampering with critical kernel structures, which as a result would drastically simplify the process of developing bootkits/rootkits.</li>
+                <li>Elected as a member of Team Slovakia 🇸🇰 , primarily solving reverse engineering tasks with a secondary focus on crypto/forensics.</li> 
+                <li>Founded and captained <a href="https://ctftime.org/team/383658" target="_blank">rakuz4n</a>, a CTF team consisting of players from teams from all across the world, with the intent on creating a friendly enviroment for collective learning and individual growth.</li>
+                <li>Closely worked with private security researchers and veteran red teamers on the topics of penetration testing and malware development, where we specifically worked on innovative methods to extract SSNs without relying on incrementation and parsing the NTDLL.dll along with anti-sandbox techniques. One of our biggest researches was the attempted identification and termination of all <a href="https://en.wikipedia.org/wiki/Kernel_Patch_Protection" target="_blank">PatchGuard (KPP)</a> threads, in order to allow tampering with critical kernel structures, which as a result would drastically simplify the process of developing bootkits/rootkits.</li>
                 <li>Led offensive team operations in custom labs against secured networks, where I focused on Active Directory, deploying a wide variety of sophisticated targeted attacks against Windows enviroments.</li>
                 <li>Participated in cybersecurity competitions hosted by Binary Confidence - where I was elected the MVP of my team due to leadership skills, team contribution and coordination. Learned valuable and hands on skills from the areas of digital forensics, proactive and reactive incident response along with threat hunting.</li>
-                <li>Placed in the top 10 of CyberGame 2025 - the national qualifier for Team Slovakia</li>
+                <li>Placed in the top 10 of <a href="https://cybergame.sk/sk" target="_blank">CyberGame</a> 2025 - the national qualifier for Team Slovakia</li> 
             </ul>
           </div>
         </div>
